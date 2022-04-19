1a85775855474eb2a6ca1a50e0b9b1a3ee439f9f
Web Design Technologies <luquinhasbritogba@hotmail.com>
2022-04-19 12:07:10 -0300

    Update

---
+++ assets/imgs/index.js
@@ -0,0 +1,6 @@
+import todayImage from './today.jpg';
+import tomorrowImage from './tomorrow.jpg';
+import weekImage from './week.jpg';
+import monthImage from './month.jpg';
+
+export {todayImage, tomorrowImage, weekImage, monthImage};
